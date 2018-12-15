69ac688bad92c78fa0206407e71bf3d6a3870af9
houxiangrui <houxiangrui@baidu.com>
2018-12-15 12:37:50 +0800

    get the react framework

---
+++ app/components/hello.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './hello.less'
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div className="hello-component">
+                edit your content in app/component/hello.js
+            </div>
+        );
+    }
+}
+
+export default Hello;
